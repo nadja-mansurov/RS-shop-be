fb970847c0e8425f4b5a99584107dbded9ab741a
Nadja Mansurov <nadja.mansurov@gmail.com>
2021-08-29 12:24:25 +0200

    add error handler

--- product-service/src/functions/product/getProductsById.ts
+++ product-service/src/functions/product/getProductsById.ts
@@ -9,15 +9,26 @@ import { ORIGIN } from '../../libs/api.constant';
 
 
 export const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof ProductsById> = async (event) => {
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": ORIGIN,
-        "Access-Control-Allow-Methods": "OPTIONS,GET"
-        },
-      body: JSON.stringify(PRODUCTS.find(item => item.id === event.pathParameters?.id))
-    };
+    const product = PRODUCTS.find(item => item.id === event.pathParameters?.id);
+    if (product)
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": ORIGIN,
+          "Access-Control-Allow-Methods": "OPTIONS,GET"
+          },
+        body: JSON.stringify(product)
+      };
+      return {
+        statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": ORIGIN,
+          "Access-Control-Allow-Methods": "OPTIONS,GET"
+          },
+        body: 'Product not found'
+      };
   }
 
 
